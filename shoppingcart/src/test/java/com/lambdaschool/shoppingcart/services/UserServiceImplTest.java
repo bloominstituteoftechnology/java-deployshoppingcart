408bcdfd8144a3961cd52b79a3e029a9c4d1944f
Lauren Emick <laurenemick6@gmail.com>
2020-08-20 17:05:54 -0700

    errors fixed

--- shoppingcart/src/test/java/com/lambdaschool/shoppingcart/services/UserServiceImplTest.java
+++ shoppingcart/src/test/java/com/lambdaschool/shoppingcart/services/UserServiceImplTest.java
@@ -1,22 +1,16 @@
 package com.lambdaschool.shoppingcart.services;
 
 import com.lambdaschool.shoppingcart.ShoppingcartApplication;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
-import org.mockito.MockitoAnnotations;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
-//import static org.junit.Assert.*;
+import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = ShoppingcartApplication.class)
@@ -29,32 +23,32 @@ public class UserServiceImplTest
     {
     }
 
-    After
+    @After
     public void tearDown() throws Exception
     {
     }
 
-    @org.junit.Test
+    @Test
     public void findAll()
     {
     }
 
-    @org.junit.Test
+    @Test
     public void findUserById()
     {
     }
 
-    @org.junit.Test
+    @Test
     public void findByName()
     {
     }
 
-    @org.junit.Test
+    @Test
     public void delete()
     {
     }
 
-    @org.junit.Test
+    @Test
     public void save()
     {
     }
