e8b97dd34fea825a01d67b5a5e13216ed2e38d27
Tarah <tarah.agbokhana@gmail.com>
2021-04-23 02:35:50 -0400

    MVP

--- shoppingcart/shoppingcart/src/main/java/com/lambdaschool/shoppingcart/services/ProductServiceImpl.java
+++ shoppingcart/shoppingcart/src/main/java/com/lambdaschool/shoppingcart/services/ProductServiceImpl.java
@@ -123,4 +123,9 @@ public class ProductServiceImpl
 
         return productrepos.save(currentProduct);
     }
+
+    @Override
+    public void deleteAll() {
+        productrepos.deleteAll();
+    }
 }
